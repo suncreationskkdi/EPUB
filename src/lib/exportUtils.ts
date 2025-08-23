9e4cb857388bcd845a89b787a9dfe7f30c3aa972
suncreationskkdi <suncreations.opensource@gmail.com>
2025-08-23 21:47:46 +0530

    Fix html2canvas import error

--- src/lib/exportUtils.ts
+++ src/lib/exportUtils.ts
@@ -1,5 +1,6 @@
 import PDFDocument from 'pdfkit';
 import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 import { BookDetails, Chapter } from '../types';
